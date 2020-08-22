6d38d4fcad0a3667e610007bf7985ae562f582c3
Lokesh <44214988+LokeshMaradi@users.noreply.github.com>
2020-08-22 20:36:31 +0530

    Create Array in Java.java

---
+++ Array in Java.java
@@ -0,0 +1,18 @@
+class array
+{
+ int[] x=new int[]{1,2,3,4,4};
+
+ public void display()
+ { int i;
+  for(i=0;i<x.length;i++){System.out.print(x[i]+"\n");}
+}
+}
+public class Arr
+{
+ public static void main(String Arg[])
+ { 
+  array a=new array();
+  a.display();
+ }
+}
+  
